4da991e6b8eb370c7f47c189a7fceb8ba2d18bc5
Ethan Henderson <ethan@zbee.me>
2020-08-19 19:51:22 -0600

    Stop setting reloading, add documentation

--- hinter/settings.py
+++ hinter/settings.py
@@ -6,32 +6,46 @@ class Settings:
     settings_file = './data/settings.dat'
     region = 'na1'
     active_user = ''
+    settings_loaded = False
 
-    def load_settings(self):
+    def load_settings(self, refresh: bool = False):
+        # Skip loading of settings if they are already loaded
+        if not refresh and self.settings_loaded:
+            print('hinter.Settings: settings already loaded')
+            return
+
         # Verify folder and file exist
         if not os.path.exists('./data/'):
             os.mkdir('./data')
-
         if not os.path.exists(self.settings_file):
             open(self.settings_file, "w+")
             return
 
+        # Open settings file
         settings_file = open(self.settings_file, "r").readlines()
 
+        # Read settings file
         for setting in settings_file:
             # Skip blank lines
             if setting == '\n':
                 continue
 
+            # Read the setting into variables
             setting = setting.split('=')
             attribute = setting[0]
             value = setting[1].split('\n')[0]
 
+            # Save the setting to the Class
+            # This has to be verbose otherwise falsey values will trip it
             if getattr(self, attribute, False) != False:
                 setattr(self, attribute, value)
 
+        # Save a setting that the settings were loaded
+        self.settings_loaded = True
+
     def write_setting(self, setting, value):
         # Remove the setting if currently set
+        # This has to be verbose otherwise falsey values will trip it
         if getattr(self, setting, False) != False:
             settings_file_original = open(self.settings_file, "r").readlines()
             lines = []
