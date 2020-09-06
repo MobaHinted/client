4c4221a46838f8ad945acf88653bd6c73f29844f
Ethan Henderson <ethan@zbee.me>
2020-09-05 22:14:33 -0600

    Getting back to match history display with cassiopeia

--- hinter/matchhistory.py
+++ hinter/matchhistory.py
@@ -2,30 +2,37 @@ from tkinter import *
 
 import hinter
 import hinter.ui.main
+import hinter.struct.user
+
+import cassiopeia
 
 
 class MatchHistory:
-    games = []
-    rank = ""
+    games: cassiopeia.MatchHistory = []
+    rank: cassiopeia.Rank = ""
     average_kda = ""
     level = 0
     icon = 0
     username = ""
 
-    def __init__(self):
+    def __init__(self, user: hinter.struct.user.User = None):
         # Load summoner information
-        user = hinter.struct.user.User(hinter.settings.active_user)
+        if user is not None:
+            user = cassiopeia.get_summoner(name=user.username)
+        else:
+            user = cassiopeia.get_summoner(name=hinter.settings.active_user)
 
-        # Don't fail on match list loading just because there is not active user selected
-        if not user.user_exists:
-            return
+        # Save variables
+        self.games = user.match_history
+        self.rank = user.ranks[cassiopeia.Queue.ranked_solo_fives]
+        self.level = user.level
+        self.icon = user.profile_icon
 
         # Load match history information
-        entries = hinter.watcher.match.matchlist_by_account(
-            hinter.settings.settings.region,
-            user.account_id
-        )
-        for match in entries['matches'][0:5]:
-            match = hinter.watcher.match.by_id(hinter.settings.region, match['gameId'])
-            text = Label(hinter.ui.main.UI.root, text=match['gameId'])
+        entries: cassiopeia.MatchHistory = user.match_history
+
+        for match in entries[0:5]:
+            match: cassiopeia.Match
+            print(match.id)
+            text = Label(hinter.ui.main.UI.root, text=match.id)
             text.pack()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import hinter.ui.main
 import hinter.matchhistory
 
+hinter.matchhistory.MatchHistory()
+
 ###
 
 hinter.ui.main.UI.root.mainloop()
