490ab16057c78f192f8e12c2ec5c3d97a4eed142
Ethan Henderson <ethan@zbee.me>
2020-09-19 03:46:59 -0600

    actually parsing basic game data

--- hinter/matchhistory.py
+++ hinter/matchhistory.py
@@ -9,6 +9,7 @@ import cassiopeia
 
 class MatchHistory:
     games: cassiopeia.MatchHistory
+    games_shown: int = 25
     rank: cassiopeia.Rank
     average_kda: float
     level = 0
@@ -24,6 +25,7 @@ class MatchHistory:
             user = cassiopeia.get_summoner(name=user.username)
         else:
             user = cassiopeia.get_summoner(name=hinter.settings.active_user)
+        self.username = user.name
 
         # Save variables
         self.games = user.match_history
@@ -42,7 +44,7 @@ class MatchHistory:
 
         # Set up the left-bar
         self.left_bar = Frame(master=hinter.ui.main.UI.screen)
-        text = Label(master=self.left_bar, text='rank, icon, level, lp, played-with stats here')
+        text = Label(master=self.left_bar, text='rank, icon, level, lp, users-played-with stats here')
         text.grid()
         self.left_bar.grid(row=0, column=0, padx=20)
 
@@ -65,23 +67,41 @@ class MatchHistory:
 
     def display_matches(self):
         # Loop through the first games
-        for key, match in enumerate(self.games[0:25]):
-            # Set up each game
+        for key, match in enumerate(self.games[0:self.games_shown]):
+            # Set up each game's container, and cast multiple variables
             game = Frame(self.history)
             match: cassiopeia.Match
+            team: str
+            player: cassiopeia.core.match.Participant
+            player_stats: cassiopeia.core.match.ParticipantStatsData
 
-            text = Label(
+            # Determine stats of user whose match history this is
+            for participant in match.participants:
+                if participant.summoner.name == self.username:
+                    team = participant.side.name
+                    player = participant
+                    player_stats = participant.stats
+
+            # Cast and determine slightly more complex variables
+            if match.is_remake:
+                win = 'remake'
+            else:
+                win = 'win' if player.stats.win else 'loss'
+
+            # Display the data in the game container
+            key_data = Label(
                 master=game, text='key: ' + str(key) + ', match: ' + str(match.id)
             )
-            text.grid(row=0, column=0)
+            key_data.grid(row=0, column=0)
 
-            text = Label(
-                master=game, text='test'
-            )
-            text.grid(row=0, column=1)
+            outcome = Label(master=game, text=win)
+            outcome.grid(row=1, column=0)
+
+            champion_played = Label(master=game, text=player.champion.name)
+            champion_played.grid(row=1, column=1)
 
             # Save each game to the grid
-            game.grid(row=key)
+            game.grid(row=key, pady=10)
 
     def __del__(self):
         hinter.ui.main.UI.clear_screen()
