907d3da0d364b0a2fa45adfbb1a690461764609e
Ethan Henderson <ethan@zbee.codes>
2023-10-02 03:22:46 -0600

    Rewrite to imgui
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

--- hinter/ui/progress.py
+++ hinter/ui/progress.py
@@ -1,30 +1,47 @@
-from tkinter import *
-from tkinter.ttk import *
+import dearpygui.dearpygui
 
 
 class Progress:
-    popup: Toplevel = None
-    bar: Progressbar = None
     current_percentage: int = 0
-    status_text: Label = None
+    imgui: dearpygui.dearpygui = dearpygui.dearpygui
 
-    def __init__(self, percentage: int, current_status: str):
+    def __init__(self, percentage: int, title: str, current_status: str):
         # Make sure percentage is 1-100
         if not 0 <= percentage <= 100:
             print('hinter.ui.Progress: progress percentage not usable')
             return
 
         self.current_percentage = percentage
-        self.popup = Toplevel()
 
-        Label(self.popup, text='Downloading Game data ...').grid(row=0, column=0, padx=50)
-        self.status_text = Label(self.popup, text=current_status)
-        self.status_text.grid(row=3, column=0, padx=20)
+        width = 325
+        height = 115
 
-        self.bar = Progressbar(self.popup, orient=HORIZONTAL, length=100, mode='determinate')
-        self.bar['value'] = self.current_percentage
-        self.bar.grid(row=2, column=0)
-        self.bar.update()
+        # Show the popup
+        with self.imgui.window(
+                label=title,
+                modal=True,
+                tag='progress-popup',
+                width=width,
+                height=height,
+                no_resize=True,
+                no_move=True,
+                no_collapse=True,
+                on_close=self.close,
+                pos=[
+                    int(self.imgui.get_viewport_width() / 2 - width / 2),
+                    int(self.imgui.get_viewport_height() / 2 - height / 2),
+                ]
+        ):
+            self.imgui.add_text(tag='progress-status', default_value=current_status, wrap=width-20)
+
+            self.imgui.add_spacer(tag='progress-spacer', height=10)
+
+            self.imgui.add_progress_bar(
+                tag='progress-bar',
+                parent='progress-popup',
+                default_value=self.current_percentage,
+                width=-1,
+            )
 
     def update(self, percentage: int, current_status: str):
         # Make sure percentage is 1-100
@@ -33,12 +50,14 @@ class Progress:
             return
 
         self.current_percentage = percentage
-        self.status_text.config(text=current_status)
-        self.bar['value'] = self.current_percentage
+        self.imgui.configure_item(item='progress-status', default_value=current_status)
+        self.imgui.set_value('progress-bar', 1/100 * self.current_percentage)
 
-        self.bar.grid(row=2, column=0)
-        self.status_text.grid(row=3, column=0, padx=20)
-        self.bar.update()
-
+    # Ensure the popup can close itself
     def close(self):
-        self.popup.destroy()
+        self.imgui.delete_item(item='progress-popup')
+        self.imgui.delete_item(item='progress-status')
+        self.imgui.delete_item(item='progress-spacer')
+        self.imgui.delete_item(item='progress-bar')
+        self.imgui.delete_item(item='progress-spacer-2')
+        self.imgui.delete_item(item='progress-info')
--- hinter/users.py
+++ hinter/users.py
@@ -1,7 +1,5 @@
 import os
 from typing import List
-from tkinter import messagebox
-from tkinter import simpledialog
 
 import hinter.struct.user
 
@@ -10,7 +8,7 @@ class Users:
     users_list = './data/users.dat'
     current_list_cache: List[hinter.struct.user.User] = []
 
-    def list_users(self, root):
+    def list_users(self, ui=None) -> List[hinter.struct.user.User]:
         # Open user file
         if not os.path.exists('./data/'):
             os.mkdir('./data')
@@ -32,28 +30,70 @@ class Users:
                 user_list.append(
                     hinter.struct.user.User(username)
                 )
-            else:
-                self.remove_user(root, username)
+            elif ui is not None:
+                self.remove_user(ui, username)
 
         # Save user list
         self.current_list_cache = user_list
 
         return user_list
 
-    def add_user(self, root, username: str = ''):
+    def add_user(self, ui, username: str = ''):
+        # Make sure the username popup can close itself
+        def close_popup():
+            ui.imgui.delete_item(item='add-user')
+            ui.imgui.delete_item(item='add-user-input')
+            ui.imgui.delete_item(item='add-user-spacer')
+            ui.imgui.delete_item(item='add-user-button')
+
+        # Retry the function, with the provided username
+        def submit(sender, data):
+            # Correct the data-source if the button was used
+            if sender == 'add-user-button':
+                data = ui.imgui.get_value('add-user-input')
+
+            # Re-run the method, with a username
+            close_popup()
+            self.add_user(ui, data)
+
         if username == '':
-            # Grab username
-            username = simpledialog.askstring(
-                'Add User',
-                'What is the username? (currently using region: '
-                + hinter.settings.region + ')',
-                parent=root
-            )
+            # Grab username in a popup
+            width = 165
+            height = 100
+            with ui.imgui.window(
+                    label='Add a User',
+                    modal=True,
+                    tag='add-user',
+                    width=width,
+                    height=height,
+                    no_resize=True,
+                    no_move=True,
+                    no_collapse=True,
+                    on_close=close_popup
+            ):
+                ui.imgui.add_input_text(
+                    tag='add-user-input',
+                    parent='add-user',
+                    on_enter=True,
+                    callback=submit,
+                    width=width,
+                    hint='League Name',
+                )
+                ui.imgui.add_spacer(tag='add-user-spacer', height=10)
+                ui.imgui.add_button(
+                    tag='add-user-button',
+                    parent='add-user',
+                    label='Add',
+                    callback=submit,
+                    width=width,
+                )
+            ui.center_window('add-user', [width, height])
+            return
 
         # Check for duplicate in current user list
         for user in self.current_list_cache:
             if user.username == username:
-                messagebox.showwarning(
+                ui.show_info_popup(
                     'Duplicate username',
                     'This account is already in your list!'
                 )
@@ -62,7 +102,7 @@ class Users:
         # Check user exists on Riot's side
         user = hinter.struct.user.User(username)
         if not user.user_exists:
-            messagebox.showwarning(
+            ui.show_info_popup(
                 'Nonexistent user',
                 'This account does not exist in this region!'
             )
@@ -76,14 +116,60 @@ class Users:
         user_file.close()
         self.current_list_cache.append(user)
 
-    def remove_user(self, root, username: str = ''):
+        # Redraw the menu
+        ui.add_menu()
+
+    def remove_user(self, ui, username: str = ''):
+        # Make sure the username popup can close itself
+        def close_popup():
+            ui.imgui.delete_item(item='remove-user')
+            ui.imgui.delete_item(item='remove-user-input')
+            ui.imgui.delete_item(item='remove-user-spacer')
+            ui.imgui.delete_item(item='remove-user-button')
+
+        # Retry the function, with the provided username
+        def submit(sender, data):
+            # Correct the data-source if the button was used
+            if sender == 'remove-user-button':
+                data = ui.imgui.get_value('remove-user-input')
+
+            # Re-run the method, with a username
+            close_popup()
+            self.remove_user(ui, data)
+
         # Grab username
         if username == '':
-            username = simpledialog.askstring(
-                'Remove User',
-                'What username do you want removed?',
-                parent=root
-            )
+            width = 165
+            height = 100
+            with ui.imgui.window(
+                    label='Remove a User',
+                    modal=True,
+                    tag='remove-user',
+                    width=width,
+                    height=height,
+                    no_resize=True,
+                    no_move=True,
+                    no_collapse=True,
+                    on_close=close_popup
+            ):
+                ui.imgui.add_input_text(
+                    tag='remove-user-input',
+                    parent='remove-user',
+                    on_enter=True,
+                    callback=submit,
+                    width=width,
+                    hint='League Name',
+                )
+                ui.imgui.add_spacer(tag='remove-user-spacer', height=10)
+                ui.imgui.add_button(
+                    tag='remove-user-button',
+                    parent='remove-user',
+                    label='Remove',
+                    callback=submit,
+                    width=width,
+                )
+            ui.center_window('remove-user', [width, height])
+            return
 
         removed = False
 
@@ -93,7 +179,7 @@ class Users:
             if user.username == username:
                 found = True
         if not found:
-            messagebox.showwarning(
+            ui.show_info_popup(
                 'Nonexistent username',
                 'This account was never added, and has not been removed!'
             )
@@ -115,7 +201,7 @@ class Users:
 
         # Throw an error if nothing could be removed
         if not removed:
-            messagebox.showwarning(
+            ui.show_info_popup(
                 'Nonexistent username',
                 'This account could not be found, and has not been removed!'
             )
@@ -131,10 +217,13 @@ class Users:
         user_list_file.close()
 
         # Confirm user was removed and reload menu
-        messagebox.showinfo(
+        ui.show_info_popup(
             'User removed',
             'This account was removed!'
         )
+
+        # Redraw the menu
+        ui.add_menu()
 
     def select_user(self, username: str):
         # Write the variable to have an active user
