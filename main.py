86bda5416e7626d47c4545af271c87d46be03071
Ethan Henderson <ethan@zbee.codes>
2023-10-03 07:10:23 -0600

    Initial setup (login) flow
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import hinter.ui.main as ui
 import hinter.matchhistory
-import hinter.settings
 
-UI = ui.UI
 
-match_history = hinter.matchhistory.MatchHistory()
-match_history.show_match_screen()
+def continue_drawing(ui, render):
+    match_history = hinter.matchhistory.MatchHistory(ui)
+    match_history.show_match_screen(render)
 
-hinter.settings.ready_settings_window()
+    hinter.settings.ready_settings_window()
+
+
+UI = ui.UI(continue_drawing)
 
 UI.imgui.start_dearpygui()
 UI.imgui.destroy_context()
