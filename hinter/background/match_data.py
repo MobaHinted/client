c6e15f83dfa93e013273fd349e0a5cd058f01b06
Ethan Henderson <ethan@zbee.codes>
2023-10-11 20:03:11 -0600

    Building out match data
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

--- hinter/background/match_data.py
+++ hinter/background/match_data.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 from typing import TypedDict, Required, Union
 
 import cassiopeia
+import pytz
+import timeago
 
 import hinter
 
@@ -62,11 +65,19 @@ class GameReturn(TypedDict, total=False):
 
 class MatchData:
     game: Union[GameReturn, None]
+    blue_team: int  # The keys to use when splitting the match data by team
+    red_team: int
     _match: cassiopeia.core.match.Match
 
     def __init__(self, game: int, user: str = None):
         self.game = None
         self._match = hinter.cassiopeia.get_match(game, hinter.settings.region)
+
+        # Just done here programmatically, so it's easier to adjust for if ever needed,
+        # or possibly to aid in supporting other game modes in the future
+        teams = [cassiopeia.data.Side.blue, cassiopeia.data.Side.red]
+        self.blue_team = teams.index(cassiopeia.data.Side.blue)
+        self.red_team = teams.index(cassiopeia.data.Side.red)
 
         self._format_game()
 
@@ -77,10 +88,20 @@ class MatchData:
         self.game = GameReturn(
             match_id=self._match_id,
             map_id=self._map_id,
+            queue=self._queue,
+            match_duration=self._match_duration,
         )
 
     def _format_game_for(self, user) -> None:
         pass
+
+    def _assemble_in_teams(self, blue_team_data: list, red_team_data: list) -> list:
+        teamed_data = []
+        # Again done here like this to adjust or support other game modes
+        teamed_data[self.blue_team] = blue_team_data
+        teamed_data[self.red_team] = red_team_data
+
+        return teamed_data
 
     @property
     def _match_id(self) -> int:
@@ -89,3 +110,60 @@ class MatchData:
     @property
     def _map_id(self) -> int:
         return self._match.map.id
+
+    @property
+    def _queue(self) -> str:
+        # noinspection PyBroadException
+        try:
+            queue = self._match.queue.name
+        except Exception:
+            return ''
+
+        # Shorter names for the queue static data
+        ranked = cassiopeia.data.Queue.ranked_solo_fives
+        flex = cassiopeia.data.Queue.ranked_flex_fives
+        clash = cassiopeia.data.Queue.clash
+        blind = cassiopeia.data.Queue.blind_fives
+        draft = cassiopeia.data.Queue.normal_draft_fives
+        aram = cassiopeia.data.Queue.aram
+        arena = cassiopeia.data.Queue.rings_of_wrath
+
+        if self._match.queue == ranked or self._match.queue == ranked.name:
+            queue = 'Ranked Solo'
+        elif self._match.queue == flex or self._match.queue == flex.name:
+            queue = 'Ranked Flex'
+        elif self._match.queue == clash or self._match.queue == clash.name:
+            queue = 'Clash'
+        elif self._match.queue == blind or self._match.queue == blind.name:
+            queue = 'Normal Blind'
+        elif self._match.queue == draft or self._match.queue == draft.name:
+            queue = 'Normal Draft'
+        elif self._match.queue == aram or self._match.queue == aram.name:
+            queue = 'ARAM'
+        elif self._match.queue == arena or self._match.queue == arena.name:
+            queue = 'Arena'
+        else:
+            queue = queue.replace('_', ' ').title()
+
+        return queue
+
+    @property
+    def _match_duration(self) -> str:
+        match_length_parts = str(self._match.duration).split(':')
+        match_minutes = int(match_length_parts[0] * 60)
+        match_minutes += int(match_length_parts[1])
+        match_minutes += int(match_length_parts[2]) / 60
+        self._match_minutes = round(match_minutes, 2)
+
+        # Calculate when the match happened
+        now = datetime.now()
+        now = pytz.utc.localize(now)
+        match_time = datetime.fromisoformat(
+            str(self._match.creation)
+        )
+
+        # Format match timing and how long ago it was
+        # duration = str(self._match.duration) + ' long - '  # HH:MM:SS display
+        duration = f'{match_minutes:>2.0f}min - '
+        duration += timeago.format(match_time, now)
+        return duration.replace('utes', '')
