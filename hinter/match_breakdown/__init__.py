1b39fdbdb738884ced677b3d2e0851fc221a78bb
Ethan Henderson <ethan@zbee.codes>
2023-10-13 04:30:41 -0600

    Add some styling using some of the new match data
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

--- hinter/match_breakdown/__init__.py
+++ hinter/match_breakdown/__init__.py
@@ -1,5 +1,5 @@
 import hinter
-import roleidentification.utilities as casiopeia_role_identification
+import cassiopeia
 
 
 class MatchBreakdown:
@@ -23,3 +23,48 @@ class MatchBreakdown:
         hinter.imgui.add_text(f'Match Breakdown: {self.match_id}', parent='match_breakdown')
         hinter.imgui.add_separator(parent='match_breakdown')
         hinter.imgui.add_text(f'Map: {self.match["map_id"]}', parent='match_breakdown')
+
+        with hinter.imgui.theme(tag='blue_team_bans-theme'):
+            with hinter.imgui.theme_component(hinter.imgui.mvImageButton, enabled_state=False):
+                hinter.imgui.add_theme_style(hinter.imgui.mvStyleVar_FrameBorderSize, 2, 2)
+                hinter.imgui.add_theme_style(hinter.imgui.mvStyleVar_FramePadding, 1, 1)
+                hinter.imgui.add_theme_color(hinter.imgui.mvThemeCol_Border, hinter.data.constants.TEAM_BLUE_COLOR)
+                hinter.imgui.add_theme_color(hinter.imgui.mvThemeCol_Button, (0, 0, 0, 0))
+                hinter.imgui.add_theme_color(hinter.imgui.mvThemeCol_ButtonActive, (0, 0, 0, 0))
+                hinter.imgui.add_theme_color(hinter.imgui.mvThemeCol_ButtonHovered, (0, 0, 0, 0))
+                hinter.imgui.add_theme_color(hinter.imgui.mvThemeCol_BorderShadow, (0, 0, 0, 0))
+        with hinter.imgui.theme(tag='red_team_bans-theme'):
+            with hinter.imgui.theme_component(hinter.imgui.mvImageButton, enabled_state=False):
+                hinter.imgui.add_theme_style(hinter.imgui.mvStyleVar_FrameBorderSize, 2, 2)
+                hinter.imgui.add_theme_style(hinter.imgui.mvStyleVar_FramePadding, 1, 1)
+                hinter.imgui.add_theme_color(hinter.imgui.mvThemeCol_Border, hinter.data.constants.TEAM_RED_COLOR)
+                hinter.imgui.add_theme_color(hinter.imgui.mvThemeCol_Button, (0, 0, 0, 0))
+                hinter.imgui.add_theme_color(hinter.imgui.mvThemeCol_ButtonActive, (0, 0, 0, 0))
+                hinter.imgui.add_theme_color(hinter.imgui.mvThemeCol_ButtonHovered, (0, 0, 0, 0))
+                hinter.imgui.add_theme_color(hinter.imgui.mvThemeCol_BorderShadow, (0, 0, 0, 0))
+
+        with hinter.imgui.group(parent='match_breakdown', horizontal=True):
+            for ban in self.match['teams_bans'][self.blue_team]:
+                image = hinter.imgui.add_image_button(
+                    ban,
+                    width=hinter.data.constants.ICON_SIZE_BAN[0],
+                    height=hinter.data.constants.ICON_SIZE_BAN[1],
+                    enabled=False,
+                )
+                hinter.imgui.bind_item_theme(image, 'blue_team_bans-theme')
+        with hinter.imgui.group(parent='match_breakdown', horizontal=True):
+            for ban in self.match['teams_bans'][self.red_team]:
+                image = hinter.imgui.add_image_button(
+                    ban,
+                    width=hinter.data.constants.ICON_SIZE_BAN[0],
+                    height=hinter.data.constants.ICON_SIZE_BAN[1],
+                    enabled=False,
+                )
+                hinter.imgui.bind_item_theme(image, 'red_team_bans-theme')
+
+        with hinter.imgui.group(parent='match_breakdown', horizontal=True):
+            for item in self.match['players_items'][self.red_team][4][0:4]:
+                hinter.imgui.add_image(item, width=hinter.data.constants.ICON_SIZE_ITEM[0])
+        with hinter.imgui.group(parent='match_breakdown', horizontal=True):
+            for item in self.match['players_items'][self.red_team][4][4:8]:
+                hinter.imgui.add_image(item, width=hinter.data.constants.ICON_SIZE_ITEM[0])
