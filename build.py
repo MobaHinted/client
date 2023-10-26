f70a2811f30809c3132aa56b7631021ca601930f
Ethan Henderson <ethan@zbee.codes>
2023-10-26 15:42:41 -0600

    Overwrite existing build
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ import os
 
 os.system('pip install -U pyinstaller pyclean')
 os.system('pyinstaller --onefile --noconsole -p=.\\venv\\Lib\\site-packages -i .\\assets\\logo.ico main.py')
-os.system('copy .\\dist\\main.exe .\\MobaHinted.exe /a')
+os.system('copy .\\dist\\main.exe .\\MobaHinted.exe /a /y')
 os.system('rmdir /s /q .\\build\\, .\\dist\\')
 os.system('pyclean .\\hinter')
 os.system('del /f /q .\\main.spec')
