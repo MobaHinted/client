031f9e945c61f81d88c36233449a5c793e93b4ee
Ethan Henderson <ethan@zbee.codes>
2023-11-01 04:19:40 -0600

    Note near-future work
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ try:
         faulthandler.enable()
 except RuntimeError:
     pass
+# TODO: Catch these errors `Windows Fatal Error: access violation (0xc0000005)`:
+#   https://stackoverflow.com/a/74601939/1843510
+#   probably in `launcher.py`. Just relaunch the application if one occurs, and log it.
+#     They're intermittently coming from `dpg.add_image` and can be ignored
 # endregion Show access violations
 
 import hinter
