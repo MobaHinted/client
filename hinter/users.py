f313f45c4352a94a55e8c41f37ce395fbd78c97a
Ethan Henderson <ethan@zbee.codes>
2023-11-01 04:22:58 -0600

    Use correct imgui instance
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

--- hinter/users.py
+++ hinter/users.py
@@ -40,16 +40,16 @@ class Users:
     def add_user(self, username: Union[str, None] = None, show_popups: bool = True) -> int:
         # Make sure the username popup can close itself
         def close_popup():
-            hinter.UI.imgui.delete_item(item='add-user')
-            hinter.UI.imgui.delete_item(item='add-user-input')
-            hinter.UI.imgui.delete_item(item='add-user-spacer')
-            hinter.UI.imgui.delete_item(item='add-user-button')
+            hinter.imgui.delete_item(item='add-user')
+            hinter.imgui.delete_item(item='add-user-input')
+            hinter.imgui.delete_item(item='add-user-spacer')
+            hinter.imgui.delete_item(item='add-user-button')
 
         # Retry the function, with the provided username
         def submit(sender, data):
             # Correct the data-source if the button was used
             if sender == 'add-user-button':
-                data = hinter.UI.imgui.get_value('add-user-input')
+                data = hinter.imgui.get_value('add-user-input')
 
             # Re-run the method, with a username
             close_popup()
@@ -59,7 +59,7 @@ class Users:
             # Grab username in a popup
             width = 165
             height = 100
-            with hinter.UI.imgui.window(
+            with hinter.imgui.window(
                     label='Add a User',
                     modal=True,
                     tag='add-user',
@@ -71,7 +71,7 @@ class Users:
                     on_close=close_popup
             ):
                 # TODO: Update to searching by Riot IDs
-                hinter.UI.imgui.add_input_text(
+                hinter.imgui.add_input_text(
                     tag='add-user-input',
                     parent='add-user',
                     on_enter=True,
@@ -80,8 +80,8 @@ class Users:
                     hint='League Name',
                 )
                 # TODO: Update to include region/continent
-                hinter.hinter.UI.imgui.add_spacer(tag='add-user-spacer', height=10)
-                hinter.UI.imgui.add_button(
+                hinter.hinter.imgui.add_spacer(tag='add-user-spacer', height=10)
+                hinter.imgui.add_button(
                     tag='add-user-button',
                     parent='add-user',
                     label='Add',
@@ -137,16 +137,16 @@ class Users:
     def remove_user(self, username: str = ''):
         # Make sure the username popup can close itself
         def close_popup():
-            hinter.hinter.UI.imgui.delete_item(item='remove-user')
-            hinter.hinter.UI.imgui.delete_item(item='remove-user-input')
-            hinter.hinter.UI.imgui.delete_item(item='remove-user-spacer')
-            hinter.hinter.UI.imgui.delete_item(item='remove-user-button')
+            hinter.hinter.imgui.delete_item(item='remove-user')
+            hinter.hinter.imgui.delete_item(item='remove-user-input')
+            hinter.hinter.imgui.delete_item(item='remove-user-spacer')
+            hinter.hinter.imgui.delete_item(item='remove-user-button')
 
         # Retry the function, with the provided username
         def submit(sender, data):
             # Correct the data-source if the button was used
             if sender == 'remove-user-button':
-                data = hinter.hinter.UI.imgui.get_value('remove-user-input')
+                data = hinter.hinter.imgui.get_value('remove-user-input')
 
             # Re-run the method, with a username
             close_popup()
@@ -156,7 +156,7 @@ class Users:
         if username == '':
             width = 165
             height = 100
-            with hinter.hinter.UI.imgui.window(
+            with hinter.hinter.imgui.window(
                     label='Remove a User',
                     modal=True,
                     tag='remove-user',
@@ -167,7 +167,7 @@ class Users:
                     no_collapse=True,
                     on_close=close_popup
             ):
-                hinter.hinter.UI.imgui.add_input_text(
+                hinter.hinter.imgui.add_input_text(
                     tag='remove-user-input',
                     parent='remove-user',
                     on_enter=True,
@@ -175,8 +175,8 @@ class Users:
                     width=width,
                     hint='League Name',
                 )
-                hinter.hinter.UI.imgui.add_spacer(tag='remove-user-spacer', height=10)
-                hinter.hinter.UI.imgui.add_button(
+                hinter.hinter.imgui.add_spacer(tag='remove-user-spacer', height=10)
+                hinter.hinter.imgui.add_button(
                     tag='remove-user-button',
                     parent='remove-user',
                     label='Remove',
