90e1edb1ee75c6453a13959922f1212fc7557720
Michael <michael.flores12349@gmail.com>
2024-02-14 22:10:15 -0700

    Added loading window stubs

---
+++ client/ViewModels/LoadingWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace client.ViewModels;
+
+using Camille.Enums;
+using Camille.RiotGames;
+using Camille.RiotGames.AccountV1;
+using Camille.RiotGames.ChampionMasteryV4;
+
+public class LoadingWindowViewModel : ViewModelBase
+{
+  public string Greeting => "Loading...";
+  
+}
---
+++ client/Views/LoadingWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace client.Views;
+
+public partial class LoadingWindow : Window
+{
+    public LoadingWindow()
+    {
+        InitializeComponent();
+    }
+}
