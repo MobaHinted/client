e2968d4e12bd441a5820336376888315979cc019
Ethan Henderson <ethan@zbee.codes>
2024-02-17 17:54:17 -0700

    Add Account structures

---
+++ client/Models/Accounts/Account.cs
@@ -0,0 +1,26 @@
+﻿using Camille.Enums;
+
+namespace client.Models.Accounts;
+
+public struct Account
+{
+    public Guid ID { get; set; }
+    public string GameName { get; set; }
+    public string TagLine { get; set; }
+    public string RiotID { get; set; }
+
+    public PlatformRoute Region { get; set; }
+    public RegionalRoute Continent { get; set; }
+
+    public Account(string gameName, string tagLine, PlatformRoute region)
+    {
+        this.ID = Guid.NewGuid();
+
+        this.GameName = gameName;
+        this.TagLine = tagLine;
+        this.RiotID = $"{this.GameName}#{this.TagLine}";
+
+        this.Region = region;
+        this.Continent = region.ToRegional();
+    }
+}
