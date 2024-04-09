edacbc5238ab9f8b7405bf02d8d12f15698c5ab2
Ethan Henderson <ethan@zbee.codes>
2024-04-09 01:38:56 -0600

    Stubbing out classes needed for MatchData

---
+++ client/Models/Data/Matches/_Role.cs
@@ -0,0 +1,35 @@
+﻿// MobaHinted Copyright (C) 2024 Ethan Henderson <ethan@zbee.codes>
+// Licensed under GPLv3 - Refer to the LICENSE file for the complete text
+
+using Camille.Enums;
+
+namespace client.Models.Data.Matches;
+
+public enum Role
+{
+    top,
+    jungle,
+    middle,
+    bottom,
+    support,
+}
+
+public class Roles
+{
+    public static Role determine(
+        Champion champion,
+        Item[] items,
+        Spells spells,
+        Runes runes,
+        string supposedLane,
+        string supposedRole,
+        byte level,
+        short jungleMinionsKilled,
+        short minionsKilled,
+        short visionScore,
+        bool isBot = false
+    )
+    {
+        return Role.middle;
+    }
+}
